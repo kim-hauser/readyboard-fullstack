f2c37b65ca0f13f6ffba61e59971e059dac08e00
kim-hauser <knhauser1s@gmail.com>
2026-08-30 15:17:14 -0500

    Update Owner entity

--- backend/src/main/java/com/readyboard/backend/models/Owner.java
+++ backend/src/main/java/com/readyboard/backend/models/Owner.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Column;
 
 @Entity
 public class Owner {
@@ -12,6 +13,7 @@ public class Owner {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @Column(length = 100)
     private String name;
 
     public Owner() {
