92c4611590fbf5ed19e926b193cce420706910d1
kim-hauser <knhauser1s@gmail.com>
2026-08-31 23:10:03 -0500

    Add PUT endpoint for changes

--- backend/src/main/java/com/readyboard/backend/controllers/ChangeController.java
+++ backend/src/main/java/com/readyboard/backend/controllers/ChangeController.java
@@ -6,6 +6,7 @@ import com.readyboard.backend.repositories.ChangeRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,8 +33,18 @@ public class ChangeController {
         return changeRepository.findById(id).orElse(null);
     }
 
+    //TODO: Add PATCH on Tuesday for "Partial Change" + 'null handling'
+
     @PostMapping
     public Change createChange(@RequestBody Change change) {
+        return changeRepository.save(change);
+    }
+
+    // Change full-replacement
+
+    @PutMapping("/{id}")
+    public Change updateChange(@PathVariable String id, @RequestBody Change change) {
+        change.setId(id);
         return changeRepository.save(change);
     }
 }
