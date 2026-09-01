31a0ce719fa26b0b01ed62c6ef2cbaf0adb6bf40
kim-hauser <knhauser1s@gmail.com>
2026-08-31 22:27:58 -0500

    Add POST and GET by ID endpoints for changes

--- backend/src/main/java/com/readyboard/backend/controllers/ChangeController.java
+++ backend/src/main/java/com/readyboard/backend/controllers/ChangeController.java
@@ -2,7 +2,11 @@ package com.readyboard.backend.controllers;
 
 import com.readyboard.backend.models.Change;
 import com.readyboard.backend.repositories.ChangeRepository;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +25,15 @@ public class ChangeController {
     @GetMapping
     public List<Change> getAllChanges() {
         return changeRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Change getChangeById(@PathVariable String id) {
+        return changeRepository.findById(id).orElse(null);
+    }
+
+    @PostMapping
+    public Change createChange(@RequestBody Change change) {
+        return changeRepository.save(change);
     }
 }
